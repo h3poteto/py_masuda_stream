9fd5f41bb3b56ae9179a0b437b340aa7ba39a1c0
AkiraFukushima <h3.poteto@gmail.com>
2018-02-10 15:12:41 +0900

    refs #4 Create anond model

--- masuda/models/__init__.py
+++ masuda/models/__init__.py
@@ -1,4 +1,5 @@
 from masuda.models.entry import *
 from masuda.models.entry_detail import *
 from masuda.models.entry_bookmark import *
+from masuda.models.anond import *
 
---
+++ masuda/models/anond.py
@@ -0,0 +1,12 @@
+from django.db import models
+from .entry import Entry
+
+
+class Anond(models.Model):
+    entry = models.OneToOneField(Entry, on_delete=models.PROTECT)
+    content_html = models.TextField()
+    created_at = models.DateTimeField('created_at', auto_now_add=True)
+    updated_at = models.DateTimeField('updated_at', auto_now=True)
+
+    def __str__(self):
+        return self.title
