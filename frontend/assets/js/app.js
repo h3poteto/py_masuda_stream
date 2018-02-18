416dadca2eff87463a930d701209912a0b533c4e
AkiraFukushima <h3.poteto@gmail.com>
2018-02-18 18:07:41 +0900

    Add axios-progress-bar

--- frontend/assets/js/app.js
+++ frontend/assets/js/app.js
@@ -3,9 +3,13 @@ import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import 'vue-awesome/icons'
 import Icon from 'vue-awesome/components/Icon'
-
+import { loadProgressBar } from 'axios-progress-bar'
+import 'axios-progress-bar/dist/nprogress.css'
 import router from './router'
 import store from './vuex'
+
+
+loadProgressBar()
 
 Vue.component('icon', Icon)
 Vue.use(ElementUI)
