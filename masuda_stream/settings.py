c84f3e19fb0477da088c07f24ea0c9b54fe43298
AkiraFukushima <h3.poteto@gmail.com>
2018-02-23 00:07:26 +0900

    refs #6 Create user api for login user

--- masuda_stream/settings.py
+++ masuda_stream/settings.py
@@ -35,6 +35,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'frontend.apps.FrontendConfig',
     'masuda.apps.MasudaConfig',
+    'user.apps.UserConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
